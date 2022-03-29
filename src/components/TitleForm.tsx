cf6cc92fd7a58d5962b6c4bfba218f4c91d4c115
unknown <hong.gilseong@gmail.com>
2022-03-29 15:04:35 +0900

    feature: create a left color box when it focuses

--- src/components/TitleForm.tsx
+++ src/components/TitleForm.tsx
@@ -23,28 +23,33 @@ function TitleForm() {
   };
 
   return (
-    <div className="w-full overflow-hidden rounded-lg bg-white">
-      <div className="h-2 bg-purple-800" />
-      <div className="w-full p-5">
-        <div className="border-b border-gray-200">
-          <input
-            type="text"
-            placeholder="설문지 제목"
-            value={textValue.titleValue}
-            onChange={(value) => onFocusInputHandler('titleValue', value)}
-            onBlur={() => onBlurInputHandler()}
-            className="h-10 w-full text-2xl placeholder:text-2xl focus:outline-none"
-          />
-        </div>
-        <div className="border-b border-gray-200">
-          <input
-            type="text"
-            placeholder="설문지 제목"
-            value={textValue.descriptionValue}
-            onChange={(value) => onFocusInputHandler('descriptionValue', value)}
-            onBlur={() => onBlurInputHandler()}
-            className="mt-3 h-5 w-full text-sm placeholder:text-sm focus:outline-none"
-          />
+    <div className="relative w-full overflow-hidden rounded-lg bg-white">
+      <div className="flex flex-col">
+        <div className="absolute left-0 bottom-0 z-10 h-full w-2 bg-blue-500" />
+        <div className="z-50 h-2 bg-purple-800" />
+        <div className="w-full p-5">
+          <div className="border-b border-gray-200">
+            <input
+              type="text"
+              placeholder="설문지 제목"
+              value={textValue.titleValue}
+              onChange={(value) => onFocusInputHandler('titleValue', value)}
+              onBlur={() => onBlurInputHandler()}
+              className="h-10 w-full text-2xl placeholder:text-2xl focus:outline-none"
+            />
+          </div>
+          <div className="border-b border-gray-200">
+            <input
+              type="text"
+              placeholder="설문지 제목"
+              value={textValue.descriptionValue}
+              onChange={(value) =>
+                onFocusInputHandler('descriptionValue', value)
+              }
+              onBlur={() => onBlurInputHandler()}
+              className="mt-3 h-5 w-full text-sm placeholder:text-sm focus:outline-none"
+            />
+          </div>
         </div>
       </div>
     </div>
