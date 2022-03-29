3285c6e396e1c8c9ba583857a6b09c73e307a91e
unknown <hong.gilseong@gmail.com>
2022-03-29 13:41:09 +0900

    feature: create the title form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import TitleForm from './components/TitleForm';
 function App() {
   return (
-    <div className="h-full w-full bg-slate-600 text-green-500">
-      <p className="text-blue-700">hi</p>
+    <div className="h-full w-full bg-fuchsia-100 p-3">
+      <div className="mx-auto max-w-3xl">
+        <TitleForm />
+      </div>
     </div>
   );
 }
---
+++ src/components/TitleForm.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+interface TextValue {
+  titleValue: string;
+  descriptionValue: string;
+}
+
+function TitleForm() {
+  const [textValue, textValueSet] = useState<TextValue>({
+    titleValue: '제목 없는 설문지',
+    descriptionValue: '',
+  });
+
+  const onFocusInputHandler = (
+    name: string,
+    value: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    textValueSet({ ...textValue, [name]: value.target.value });
+  };
+  const onBlurInputHandler = () => {
+    if (textValue.titleValue.length === 0)
+      textValueSet({ ...textValue, titleValue: '제목 없는 설문지' });
+  };
+
+  return (
+    <div className="w-full overflow-hidden rounded-lg bg-white">
+      <div className="h-2 bg-purple-800" />
+      <div className="w-full p-5">
+        <div className="border-b border-gray-200">
+          <input
+            type="text"
+            placeholder="설문지 제목"
+            value={textValue.titleValue}
+            onChange={(value) => onFocusInputHandler('titleValue', value)}
+            onBlur={() => onBlurInputHandler()}
+            className="h-10 w-full text-2xl placeholder:text-2xl focus:outline-none"
+          />
+        </div>
+        <div className="border-b border-gray-200">
+          <input
+            type="text"
+            placeholder="설문지 제목"
+            value={textValue.descriptionValue}
+            onChange={(value) => onFocusInputHandler('descriptionValue', value)}
+            onBlur={() => onBlurInputHandler()}
+            className="mt-3 h-5 w-full text-sm placeholder:text-sm focus:outline-none"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TitleForm;
